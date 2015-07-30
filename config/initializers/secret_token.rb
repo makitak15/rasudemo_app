02bbddbabbebe26a21f18cc232c61530e024ab9a
Maki <maki.takei.takahashi@gmail.com>
2015-07-30 11:12:55 +0900

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YasudemoApp::Application.config.secret_key_base = '83b7708d96fb0dd956b51de6e7b660fbc6cf6cddfc155db8f44525ce389870d433b6f88125393a41ccba8cb5340cedcf950c548ec2fdddcdcd514fbbf1b35ce9'
